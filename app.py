db7538b291184167f4c8eb4b22b7696acd32c830
Savita <savitabhati749@gmail.com>
2026-07-30 17:34:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ with open("style.css") as f:
 
 
 GROQ_API_URL = "https://api.groq.com/openai/v1/chat/completions"
-GROQ_API_KEY = "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"
+GROQ_API_KEY = "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"
 
 MODEL = "llama-3.3-70b-versatile"
 
